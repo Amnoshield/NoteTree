39866f3d182d3ddd34b67415376de776641af8ab
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2026-01-11 00:46:24 -0500

    updating vite config in hopes of fixing gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import path from 'node:path';
 
 export default defineConfig({
   root: path.resolve(__dirname, 'apps/web'),
+  base: '/NoteTree/',
   build: {
     //outDir: path.resolve(__dirname, '../dist'),
+    outDir: 'dist',
     emptyOutDir: true
   }
 });
